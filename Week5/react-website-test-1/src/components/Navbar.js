58f5ca1a1bb2596ddc686e28dc2e0bf30267e75f
Alfaben <zrav420@gmail.com>
2021-03-09 14:37:36 +0700

    Add files via upload

---
+++ Week5/react-website-test-1/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, (useState) from 'react'
+
+function Navbar(){
+    return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo"></Link>
+            </div>
+        </nav>
+    </>
+    )
+}
